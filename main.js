643e97e4bd07fe5a1f1692d5dd80e4230f5943b9
baraa <154226833+baraawael722@users.noreply.github.com>
2024-08-09 19:50:41 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+// main.js
+
+// main.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinksContainer = document.querySelector('.nav__links-container');
+
+    menuToggle.addEventListener('click', function() {
+        navLinksContainer.classList.toggle('active');
+    });
+});
+
+
